0556cfb5da594530f831455e5da73f5824e144a7
Anmol Parande <parandea1.7@gmail.com>
2022-09-30 00:36:05 -0700

    Bump version to 1.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Anmol Parande'
 author = 'Anmol Parande'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
